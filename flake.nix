c7a241132b82ac12733d786c0c001eda4a469fa8
Kaptoshka <kaptoshka@duck.com>
2026-04-03 11:08:40 +0400

    chore(flake)!: update flakes for api testing

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "E2E Autotest Framework";
+  description = "API Testing Framework";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -10,61 +10,34 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-
-        browsersJson = builtins.readFile "${pkgs.playwright-driver}/browsers.json";
-        browsersData = builtins.fromJSON browsersJson;
-        inherit (browsersData) browsers;
-
-        chromium-rev = (builtins.head
-          (builtins.filter (x: x.name == "chromium") browsers)).revision;
-
-        firefox-rev = (builtins.head
-          (builtins.filter (x: x.name == "firefox") browsers)).revision;
-
-        webkit-rev = (builtins.head
-          (builtins.filter (x: x.name == "webkit") browsers)).revision;
       in
       {
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [
+            # Reporting
             allure
 
+            # Go toolchain
             go_1_26
             gopls
             delve
-            golangci-lint
-            gotools
 
+            # Linting
+            golangci-lint
+            gotools # goimports
+            yamllint
             statix
             deadnix
             nixpkgs-fmt
-
-            yamllint
-            yamlfmt
-
-            nodejs
-            playwright-driver.browsers
           ];
 
           shellHook = ''
-            echo "Go E2E Autotest Framework"
+            echo "Go API Autotest Framework"
             echo "- Go version: $(go version)"
-            echo "- Node version: $(node --version)"
-            echo "- Chromium rev: ${chromium-rev}"
-            echo "- Firefox rev: ${firefox-rev}"
-            echo "- WebKit rev: ${webkit-rev}"
 
             export ENV_FILE="$PWD/.env"
-
-            export PLAYWRIGHT_BROWSER_PATH=${pkgs.playwright-driver.browsers}
-            export PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS=true
-            export PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD=1
-
-            export PLAYWRIGHT_NODEJS_PATH="${pkgs.nodejs}/bin/node"
-
-            export PLAYWRIGHT_CHROMIUM_EXECUTABLE_PATH="${pkgs.playwright-driver.browsers}/chromium-${chromium-rev}/chrome-linux64/chrome"
-            export PLAYWRIGHT_FIREFOX_EXECUTABLE_PATH="${pkgs.playwright-driver.browsers}/firefox-${firefox-rev}/firefox/firefox"
-            export PLAYWRIGHT_WEBKIT_EXECUTABLE_PATH="${pkgs.playwright-driver.browsers}/webkit-${webkit-rev}/pw-run.sh"
+            export ALLURE_RESULTS_DIR="$PWD/artifacts/allure-results"
+            export LOG_DIR="$PWD/artifacts/logs"
           '';
         };
       }
